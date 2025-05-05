5bf63b276f9a26fbb746f3df90bfc996494b76c8
asim1734 <asim.rupani@gmail.com>
2025-05-05 21:44:48 +0530

    Initialize React

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/log-in': 'http://localhost:3001',
+      '/register': 'http://localhost:3001',
+      '/log-out': 'http://localhost:3001',
+      '/new-member': 'http://localhost:3001',
+      '/new-message': 'http://localhost:3001',
+      '/admin-form': 'http://localhost:3001',
+    },
+  },
+});
